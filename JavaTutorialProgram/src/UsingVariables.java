6bfc60a53bb684d406ca9a448a9b9163e7db3508
github ShimomiyaYuuki <yuukiud0@gmail.com>
2021-04-10 12:53:51 +0900

    HelloWorldProgram.javaをHelloWorld.javaに変更。 UsingVariables.javaを追加。
    少しずつね。まずは慣れよう。

---
+++ JavaTutorialProgram/src/UsingVariables.java
@@ -0,0 +1,24 @@
+public class UsingVariables {
+    public static void main(String[] args) {
+        int myNumber = 88;
+        short myShort = 888;
+        long myLong = 121212;
+
+        double myDouble = 4.44;
+        float myFloat = 111.11f;
+
+        char myChar = 's';
+        boolean myBoolean = false;
+
+        byte myByte = 101;
+
+        System.out.println(myNumber);
+        System.out.println(myShort);
+        System.out.println(myLong);
+        System.out.println(myDouble);
+        System.out.println(myFloat);
+        System.out.println(myChar);
+        System.out.println(myBoolean);
+        System.out.println(myByte);
+    }
+}

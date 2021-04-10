645230cefc954c7fe0599c34aa1f36aa608b5046
github ShimomiyaYuuki <yuukiud0@gmail.com>
2021-04-10 14:05:11 +0900

    For文 最後のSystem.out.printf("The value \n of i is: $d/n", i);で%dを数字にできないのが心残り

---
+++ JavaTutorialProgram/src/ForLoops.java
@@ -0,0 +1,10 @@
+public class ForLoops {
+    public static void main(String[] args) {
+        for (int i = 1; i < 11; i++) {
+            System.out.printf("It looped for:  %d", i );
+            System.out.println();
+
+            System.out.printf("The value \n of i is: $d/n", i);
+        }
+    }
+}

ca39961a42b4070bbafa6ecc76ae1dfd55f69025
github ShimomiyaYuuki <yuukiud0@gmail.com>
2021-04-10 13:22:35 +0900

    文字のあれこれです。

---
+++ JavaTutorialProgram/src/StringsWorkingWithText.java
@@ -0,0 +1,22 @@
+public class StringsWorkingWithText {
+    public static void main(String[] args) {
+        int myInt = 8;
+        String text = "はやくJavaができるようになりたいな";
+
+        String blank = " ";
+
+        String name = "Yuki";
+
+        String greeting = text + blank + name;
+
+        System.out.println(greeting);
+
+        System.out.println("Hello" + " " + "Yuki");
+
+        System.out.println("My integer is: " + myInt);
+
+        double myDouble = 10.5;
+
+        System.out.println("My number is: " + myDouble + ".");
+    }
+}

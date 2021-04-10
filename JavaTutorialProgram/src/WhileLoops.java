ffec1b04c8e31a2b11a6742afb0405f19b7fe97d
github ShimomiyaYuuki <yuukiud0@gmail.com>
2021-04-10 13:29:37 +0900

    While文

---
+++ JavaTutorialProgram/src/WhileLoops.java
@@ -0,0 +1,14 @@
+public class WhileLoops {
+    public static void main(String[] args) {
+    
+        int value = 10;
+
+        while (value < 100) {
+
+            System.out.println("Now value is " + value);
+
+            value = value + 1;
+        }
+    }
+
+}
